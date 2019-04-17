ea8e6392257d474eb4246e637ae02294bb350d47
Daniel Diaz <diaz.carrete@facebook.com>
2019-04-17 23:05:44 +0200

    Capable

--- lib/Effectiviwonder.hs
+++ lib/Effectiviwonder.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, KindSignatures, 
+             DataKinds, PolyKinds, TypeOperators, 
+             AllowAmbiguousTypes #-}
 module Effectiviwonder () where 
+
+import Data.RBR
+import Data.Kind
+import GHC.TypeLits
+
+class Capable (env :: Type) (m :: Type -> Type) (name :: k) (c :: (Type -> Type) -> Type) where
+    getCapability :: env -> c m
+
+type family MultiCapable (env :: Type) (m :: Type -> Type) (cs :: [ (k, (Type -> Type) -> Type) ]) :: Constraint where
+    MultiCapable _   _ '[]       = ()
+    MultiCapable env m ( '(name, c) ': xs ) = (Capable env m name c, MultiCapable env m xs)
 
 
