3a731975289d79a28fbbbe54e7dd8d79bf4261da
Daniel Diaz <diaz.carrete@facebook.com>
2019-04-19 21:22:35 +0200

    mkUsers

--- tests/tests.hs
+++ tests/tests.hs
@@ -8,6 +8,7 @@
              KindSignatures,
              PartialTypeSignatures,
              FlexibleContexts,
+             AllowAmbiguousTypes,
              ScopedTypeVariables
 #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
@@ -22,11 +23,13 @@ import GHC.Generics (Generic)
 import Test.Tasty
 import Test.Tasty.HUnit (testCase,Assertion,assertEqual,assertBool)
 
---import Control.Monad.Reader
+import Control.Monad.Trans
 import Control.Monad.Trans.Reader
 
 import Effectiviwonder
 import Effectiviwonder.State
+import Effectiviwonder.Interact
+import Effectiviwonder.Yield
 
 tests :: TestTree
 tests = testGroup "Tests" [ 
@@ -51,6 +54,35 @@ modifyTwoStatesTest = do
     (r1,r2) <- runReaderT modifyTwoStates (Capabilities env)
     assertEqual "r1" 2 r1
     assertEqual "r2" 8 r2
+
+--
+--
+--
+type UserId = Int
+
+data User = User { userName :: String } deriving Show
+
+data Users m = Users {
+       _getUserById :: UserId -> m User
+    }
+
+getUserById :: forall name env m. (Monad m, Capable name env, Capability name env ~ Users m) => UserId -> ReaderT env m User
+getUserById userId =
+    do c <- getCapability @name <$> ask
+       lift $ _getUserById c userId
+
+mkUsers :: Interact UserId User IO 
+        -> Yield String IO 
+        -> State Int IO
+        -> Users IO
+mkUsers interactor yielder stateful = Users (\uid ->
+    do _yield yielder "Looking for an user" 
+       u <- _request interactor uid
+       _modify stateful succ
+       return u)
+
+--
+--
 
 main :: IO ()
 main = defaultMain tests
