a69dbc410613eed824ed91a5e0bb428ea566caca
Daniel Diaz <diaz.carrete@facebook.com>
2019-04-19 14:48:32 +0200

    Capable typeclass is too ambiguous

--- tests/tests.hs
+++ tests/tests.hs
@@ -14,7 +14,6 @@
 module Main where
 
 import Data.RBR
-import Data.SOP
 import GHC.TypeLits
 import Data.Proxy
 import Data.Kind
@@ -23,4 +22,29 @@ import GHC.Generics (Generic)
 import Test.Tasty
 import Test.Tasty.HUnit (testCase,Assertion,assertEqual,assertBool)
 
+import Control.Monad.Reader
+import Control.Monad.Trans.Reader
 
+import Effectiviwonder
+import Effectiviwonder.State
+
+tests :: TestTree
+tests = testGroup "Tests" [ 
+                                testCase "modifyTwoStates" modifyTwoStatesTest
+                          ]
+
+modifyTwoStates :: (MonadReader  env m, 
+                    MultiCapable env m '[ '("foo",State Int), '("bar",State Int) ])
+                => m (Int,Int)
+modifyTwoStates = 
+    do modify @"foo" succ
+       modify @"bar" succ
+       (,) <$> get @"foo" <*> get @"bar"
+
+modifyTwoStatesTest :: Assertion
+modifyTwoStatesTest = do
+    --let insertI @""  
+    return ()
+
+main :: IO ()
+main = defaultMain tests
