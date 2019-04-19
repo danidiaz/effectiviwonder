8bbaf10d6e2280c37fd63fc7579ea23a94990cb6
Daniel Diaz <diaz.carrete@facebook.com>
2019-04-19 12:12:20 +0200

    State

--- lib/Effectiviwonder/State.hs
+++ lib/Effectiviwonder/State.hs
@@ -1,3 +1,21 @@
-module Effectiviwonder.State () where
 
+module Effectiviwonder.State (
+    State(..),
+    mkRefBackedState
+    ) where
 
+import Effectiviwonder
+import Data.IORef
+
+data State s m = State {
+       _get :: m s 
+    ,  _set :: s -> m ()
+    ,  _modify :: (s -> s) -> m ()
+    }
+
+mkRefBackedState :: s -> IO (State s IO)
+mkRefBackedState s =
+    do ref <- newIORef s
+       return (State (readIORef ref)
+                     (writeIORef ref)
+                     (modifyIORef ref))
